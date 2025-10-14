2eabbeebcf967644d7fba5a87c1051c1d0244751
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-14 00:32:49 -0300

    Feat:Criando a classe responsavel por mapear os objetos da Categoria

--- src/main/java/org/example/movieflix/mapper/CategoriaMapper.java
+++ src/main/java/org/example/movieflix/mapper/CategoriaMapper.java
@@ -1,4 +1,25 @@
 package org.example.movieflix.mapper;
 
+import lombok.experimental.UtilityClass;
+import org.example.movieflix.entity.Categoria;
+import org.example.movieflix.request.CategoriaRequest;
+import org.example.movieflix.response.CategoriaResponse;
+
+@UtilityClass
 public class CategoriaMapper {
+
+    public static Categoria toCategoria(CategoriaRequest categoriaRequest) {
+        return Categoria
+                .builder()
+                .nome(categoriaRequest.nome())
+                .build();
+    }
+
+    public static CategoriaResponse toCategoriaResponse(Categoria categoria) {
+        return CategoriaResponse
+                .builder()
+                .id(categoria.getId())
+                .nome(categoria.getNome())
+                .build();
+    }
 }

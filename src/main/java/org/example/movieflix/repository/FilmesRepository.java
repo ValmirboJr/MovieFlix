60f058c792156c819579cd9264fcd500376e9931
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-15 18:05:30 -0300

    Feat:Criando a camada responsável pela navegação dos dados da tabela filmes

--- src/main/java/org/example/movieflix/repository/FilmesRepository.java
+++ src/main/java/org/example/movieflix/repository/FilmesRepository.java
@@ -1,4 +1,15 @@
 package org.example.movieflix.repository;
 
-public interface FilmesRepository {
+
+import org.example.movieflix.entity.Categoria;
+import org.example.movieflix.entity.Filmes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FilmesRepository extends JpaRepository<Filmes,Long> {
+
+    List<Filmes> findByCategorias(List<Categoria> categorias);
 }

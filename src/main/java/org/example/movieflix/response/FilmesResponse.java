ead7d285a6996e732bb865239ae82bd774626cf8
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-15 19:43:30 -0300

    Feat:Criando a classe responsável pelos dados que vão ser administrados pós requisições

--- src/main/java/org/example/movieflix/response/FilmesResponse.java
+++ src/main/java/org/example/movieflix/response/FilmesResponse.java
@@ -1,4 +1,21 @@
 package org.example.movieflix.response;
 
-public record FilmesResponse() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record FilmesResponse(Long id,
+                             String titulo,
+                             String descricao,
+                             @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "dd/mm/yyyy")
+                             LocalDate datalancamento,
+                             double avaliacao,
+                             List<CategoriaResponse> categoria,
+                             List<StreamingResponse> streamings) {
+
+
+
 }

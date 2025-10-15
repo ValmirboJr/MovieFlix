e8b9dbef6d85fa84e4c870bd3bd6b67dfde0a960
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-15 19:44:33 -0300

    Feat:Criando a classe responsável pelos endpoints da aplicação para os metodos CRUD

--- src/main/java/org/example/movieflix/controller/FilmesController.java
+++ src/main/java/org/example/movieflix/controller/FilmesController.java
@@ -1,4 +1,62 @@
 package org.example.movieflix.controller;
 
+import org.example.movieflix.entity.Filmes;
+import org.example.movieflix.mapper.FilmesMapper;
+import org.example.movieflix.request.FilmesRequest;
+import org.example.movieflix.response.FilmesResponse;
+import org.example.movieflix.service.FilmesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/filmes")
 public class FilmesController {
+
+    @Autowired
+    private FilmesService filmesService;
+
+    @PostMapping("/salvar")
+    public ResponseEntity<FilmesResponse> salvar(@RequestBody FilmesRequest filmesRequest) {
+        Filmes salvo = filmesService.salvar(FilmesMapper.toFilmes(filmesRequest));
+        return ResponseEntity.ok(FilmesMapper.toFilmesResponse(salvo));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FilmesResponse>> listar() {
+        return ResponseEntity.ok(filmesService.buscarTodos().stream().map(FilmesMapper::toFilmesResponse).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FilmesResponse> buscarporid(@PathVariable Long id) {
+     return filmesService.buscarFilmesporid(id)
+                .map(filme ->  ResponseEntity.ok(FilmesMapper.toFilmesResponse(filme)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FilmesResponse> atualizar(@PathVariable Long id, @RequestBody FilmesRequest filmesRequest) {
+        return filmesService.atualizar(id,FilmesMapper.toFilmes(filmesRequest))
+                .map(filmes -> ResponseEntity.ok(FilmesMapper.toFilmesResponse(filmes)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/busca")
+    public ResponseEntity<List<FilmesResponse>> buscarporcateoria(@RequestParam Long categoria) {
+        return ResponseEntity.ok(filmesService.buscarFilmesPorCategoria(categoria).stream().map(FilmesMapper::toFilmesResponse).toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        Optional<Filmes> optionalFilmes = filmesService.buscarFilmesporid(id);
+        if (optionalFilmes.isPresent()) {
+            filmesService.deletarfilme(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }

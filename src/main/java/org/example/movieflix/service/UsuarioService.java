990b86f2e30b8c8ef7fcd3abb80daf94bd50cf1f
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-16 09:26:23 -0300

    Feat:Criando a camada responsável pelos metodos de CRUD

--- src/main/java/org/example/movieflix/service/UsuarioService.java
+++ src/main/java/org/example/movieflix/service/UsuarioService.java
@@ -1,4 +1,25 @@
 package org.example.movieflix.service;
 
+import org.example.movieflix.entity.Usuario;
+import org.example.movieflix.repository.UsuarioRepository;
+import org.example.movieflix.request.UsuarioRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository  usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario registrar(Usuario usuario) {
+        String senha = usuario.getSenha();
+        usuario.setSenha(passwordEncoder.encode(senha));
+        return usuarioRepository.save(usuario);
+    }
+
 }

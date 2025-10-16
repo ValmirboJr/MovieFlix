c4235747a490db1a210cb6e12884f9c235b8d2bf
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-16 09:22:55 -0300

    Feat:Criando a camada responsável pela busca de dados na tabela usuario

--- src/main/java/org/example/movieflix/repository/UsuarioRepository.java
+++ src/main/java/org/example/movieflix/repository/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package org.example.movieflix.repository;
 
-public class UsuarioRepository {
+import org.example.movieflix.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<UserDetails> findUsuarioByEmail(String email);
 }

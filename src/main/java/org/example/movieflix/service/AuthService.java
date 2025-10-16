27b91fab9d9e3add641d6445396d8414c448f594
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-16 09:53:12 -0300

    Feat:Criando a camada responsável pela authenticação e segurança dos dados para login do usuario

--- src/main/java/org/example/movieflix/service/AuthService.java
+++ src/main/java/org/example/movieflix/service/AuthService.java
@@ -1,4 +1,20 @@
 package org.example.movieflix.service;
 
-public class AuthService {
+import org.example.movieflix.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findUsuarioByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Usuario ou senha invalido"));
+    }
 }

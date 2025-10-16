381417b4bdd1f113e46cb17d53cc4d3f10a5792b
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-16 09:34:34 -0300

    Feat:Criando a camada responsável pelo mapeamento de objetos e entidade

--- src/main/java/org/example/movieflix/mapper/UsuarioMapper.java
+++ src/main/java/org/example/movieflix/mapper/UsuarioMapper.java
@@ -1,4 +1,27 @@
 package org.example.movieflix.mapper;
 
+import lombok.experimental.UtilityClass;
+import org.example.movieflix.entity.Usuario;
+import org.example.movieflix.request.UsuarioRequest;
+import org.example.movieflix.response.UsuarioResponse;
+
+@UtilityClass
 public class UsuarioMapper {
+
+    public static Usuario toUsuario(UsuarioRequest usuarioRequest) {
+            return Usuario.builder()
+                    .nome(usuarioRequest.nome())
+                    .email(usuarioRequest.email())
+                    .senha(usuarioRequest.senha())
+                    .build();
+
+    }
+
+    public static UsuarioResponse toUsuarioResponse(Usuario usuario) {
+        return UsuarioResponse.builder()
+                .id(usuario.getId())
+                .nome(usuario.getNome())
+                .email(usuario.getEmail())
+                .build();
+    }
 }

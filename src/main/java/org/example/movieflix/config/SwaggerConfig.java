080c0a22ce5c8664d636436071b131d19d795432
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-17 09:03:29 -0300

    Feat:Criando a classe responsável pela documentação do projeto

--- src/main/java/org/example/movieflix/config/SwaggerConfig.java
+++ src/main/java/org/example/movieflix/config/SwaggerConfig.java
@@ -1,4 +1,29 @@
 package org.example.movieflix.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+
+        Info info = new Info();
+
+        Contact contact = new Contact();
+        contact.name("Valmir");
+        contact.email("valmirborgesojr@gmails.com");
+
+
+        info.setTitle("Movie Flix");
+        info.version("v1");
+        info.description("Aplicação focada em gerenciamento de um catalogo de filmes");
+        info.contact(contact);
+
+        return new OpenAPI().info(info);
+    }
 }
